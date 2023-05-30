797bc182f1c7290dfd2b3fdbf030a1bba45e156c
dubtcs <75275524+dubtcs@users.noreply.github.com>
2023-05-30 14:28:30 -0400

    premake

---
+++ premake5.lua
@@ -0,0 +1,92 @@
+
+-- for use with AxtEngine (https://github.com/dubtcs/AxtEngine)
+
+project "assimp"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+
+    targetdir ("bin/" .. output .. "/%{prj.name}")
+    objdir ("bin-int/" .. output .. "/%{prj.name}")
+
+    files {
+        "include/**",
+        "code/Common/**",
+        "code/PostProcessing/**",
+
+        "code/AssetLib/glTF/**",
+        "code/AssetLib/glTF2/**",
+    }
+
+    includedirs {
+        "code",
+        "include",
+        "contrib/irrXML",
+        "contrib/rapidjson/include",
+    }
+
+    defines {
+        "USE_STATIC_CRT",
+        "ASSIMP_BUILD_NO_X_IMPORTER",
+        "ASSIMP_BUILD_NO_3DS_IMPORTER",
+        "ASSIMP_BUILD_NO_MD3_IMPORTER",
+        "ASSIMP_BUILD_NO_MDL_IMPORTER",
+        "ASSIMP_BUILD_NO_MD2_IMPORTER",
+        "ASSIMP_BUILD_NO_PLY_IMPORTER",
+        "ASSIMP_BUILD_NO_ASE_IMPORTER",
+        "ASSIMP_BUILD_NO_OBJ_IMPORTER",
+        "ASSIMP_BUILD_NO_AMF_IMPORTER",
+        "ASSIMP_BUILD_NO_HMP_IMPORTER",
+        "ASSIMP_BUILD_NO_SMD_IMPORTER",
+        "ASSIMP_BUILD_NO_MDC_IMPORTER",
+        "ASSIMP_BUILD_NO_MD5_IMPORTER",
+        "ASSIMP_BUILD_NO_STL_IMPORTER",
+        "ASSIMP_BUILD_NO_LWO_IMPORTER",
+        "ASSIMP_BUILD_NO_DXF_IMPORTER",
+        "ASSIMP_BUILD_NO_NFF_IMPORTER",
+        "ASSIMP_BUILD_NO_RAW_IMPORTER",
+        "ASSIMP_BUILD_NO_OFF_IMPORTER",
+        "ASSIMP_BUILD_NO_AC_IMPORTER",
+        "ASSIMP_BUILD_NO_BVH_IMPORTER",
+        "ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
+        "ASSIMP_BUILD_NO_IRR_IMPORTER",
+        "ASSIMP_BUILD_NO_Q3D_IMPORTER",
+        "ASSIMP_BUILD_NO_B3D_IMPORTER",
+        "ASSIMP_BUILD_NO_COLLADA_IMPORTER",
+        "ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
+        "ASSIMP_BUILD_NO_CSM_IMPORTER",
+        "ASSIMP_BUILD_NO_3D_IMPORTER",
+        "ASSIMP_BUILD_NO_LWS_IMPORTER",
+        "ASSIMP_BUILD_NO_OGRE_IMPORTER",
+        "ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+        "ASSIMP_BUILD_NO_MS3D_IMPORTER",
+        "ASSIMP_BUILD_NO_COB_IMPORTER",
+        "ASSIMP_BUILD_NO_BLEND_IMPORTER",
+        "ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
+        "ASSIMP_BUILD_NO_NDO_IMPORTER",
+        "ASSIMP_BUILD_NO_IFC_IMPORTER",
+        "ASSIMP_BUILD_NO_XGL_IMPORTER",
+        "ASSIMP_BUILD_NO_FBX_IMPORTER",
+        "ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+        -- "ASSIMP_BUILD_NO_GLTF_IMPORTER", -- MAIN FILE FORMAT
+        "ASSIMP_BUILD_NO_C4D_IMPORTER",
+        "ASSIMP_BUILD_NO_3MF_IMPORTER",
+        "ASSIMP_BUILD_NO_X3D_IMPORTER",
+        "ASSIMP_BUILD_NO_MMD_IMPORTER",
+    }
+
+    filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
